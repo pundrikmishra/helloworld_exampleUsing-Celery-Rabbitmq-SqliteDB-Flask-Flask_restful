e3804c15cef26b38fa7738b90b7074eb7021c981
pundrikmishra <pundrikmishra1234@gmail.com>
2020-08-30 20:59:36 +0530

    done

--- application.py
+++ application.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from flask_restful import Resource, Api
-from pm import make_celery
+# from pm import make_celery
+from pm import celery ### if use one line celer code in pm.py file then uncomment this line
 
 app = Flask(__name__)
-app.config['CELERY_BROKER_URL'] = 'amqp://guest:@localhost:5672//'
-app.config['CELERY_RESULT_BACKEND'] = 'db+sqlite:///db.sqlite3'
-celery = make_celery(app)
+# app.config['CELERY_BROKER_URL'] = 'amqp://guest:@localhost:5672//'
+# app.config['CELERY_RESULT_BACKEND'] = 'db+sqlite:///db.sqlite3'
+# celery = make_celery(app)
 api = Api(app)
+
 
 class HelloWorld(Resource):
     def get(self):
--- pm.py
+++ pm.py
@@ -1,17 +1,19 @@
 from celery import Celery
+celery = Celery('main', backend='rpc://', broker='amqp://guest:@localhost:5672//')
+# You can also use above single line of code to run run celery worker OR use below code
 
-def make_celery(app):
-    celery_app = Celery(
-        app.import_name,
-        backend=app.config['CELERY_RESULT_BACKEND'],
-        broker=app.config['CELERY_BROKER_URL']
-    )
-    celery_app.conf.update(app.config)
-
-    class ContextTask(celery_app.Task):
-        def __call__(self, *args, **kwargs):
-            with app.app_context():
-                return self.run(*args, **kwargs)
-
-    celery_app.Task = ContextTask
-    return celery_app
+# def make_celery(app):
+#     celery_app = Celery(
+#         app.import_name,
+#         backend=app.config['CELERY_RESULT_BACKEND'],
+#         broker=app.config['CELERY_BROKER_URL']
+#     )
+#     celery_app.conf.update(app.config)
+#
+#     class ContextTask(celery_app.Task):
+#         def __call__(self, *args, **kwargs):
+#             with app.app_context():
+#                 return self.run(*args, **kwargs)
+#
+#     celery_app.Task = ContextTask
+#     return celery_app
